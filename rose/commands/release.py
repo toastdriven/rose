9dd87305061670ba6f78d36a32db523fc374bf71
Daniel Lindsley <daniel@toastdriven.com>
2013-06-15 21:36:14 -0700

    Working ``release`` command & more tests.

--- rose/commands/release.py
+++ rose/commands/release.py
@@ -1,9 +1,24 @@
+from __future__ import unicode_literals
+
 from rose.base import BaseCommand
 
 
 class ReleasePackage(BaseCommand):
+    def help(self, *args, **kwargs):
+        return '\n'.join([
+            "Usage: rose release",
+            "",
+            "Pushes the release to PyPI.",
+        ])
+
     def run(self, *args, **kwargs):
-        pass
+        sh = self.shell.run('python setup.py sdist')
+
+        if not self.check_success(sh):
+            return False
+
+        sh = self.shell.run('python setup.py sdist upload')
+        return self.check_success(sh)
 
 
 command_class = ReleasePackage
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -10,13 +10,25 @@ TODO
 import os
 import shutil
 
+import mock
+from shell import Shell
+
 from rose.base import Rose
-from rose.commands.bump import BumpVersion
-from rose.commands.tag import TagVersion
-from rose.commands.setup import CreatePackage
 from rose.commands.release import ReleasePackage
+from rose.commands.tag import TagVersion
 
 from .utils import BaseRoseTestCase
+
+
+class FakeShell(Shell):
+    def __init__(self, *args, **kwargs):
+        super(FakeShell, self).__init__(*args, **kwargs)
+        self.cmds = []
+
+    def run(self, cmd):
+        self.cmds.append(cmd)
+        self.code = 0
+        return self
 
 
 class BaseRoseCommandsTestCase(BaseRoseTestCase):
@@ -57,14 +69,42 @@ class RoseIntegrationTestCase(BaseRoseCommandsTestCase):
         self.assertTrue("__version__ = (1, 0, 1, 'final')" in raw_code)
 
     def test_tag_git(self):
-        runner = self.create_runner()
-        pass
+        tag = TagVersion(rose=self.create_rose())
+        faked = FakeShell()
 
-    def test_setup(self):
-        runner = self.create_runner()
-        pass
+        with mock.patch.object(tag, 'shell', faked) as fake_tag:
+            tag.run('v1.0.1')
+
+        self.assertEqual(faked.cmds, ['git tag v1.0.1'])
+
+    def test_tag_git_flow(self):
+        tag = TagVersion(rose=self.create_rose())
+        faked = FakeShell()
+
+        with mock.patch.object(tag, 'shell', faked) as fake_tag:
+            tag.run('v1.0.1', t='git-flow')
+
+        self.assertEqual(faked.cmds, [
+            'git checkout -b release-v1.0.1',
+            'git co master',
+            'git merge release-v1.0.1',
+            'git co develop',
+            'git merge master',
+        ])
 
     def test_release(self):
+        release = ReleasePackage(rose=self.create_rose())
+        faked = FakeShell()
+
+        with mock.patch.object(release, 'shell', faked) as fake_tag:
+            release.run()
+
+        self.assertEqual(faked.cmds, [
+            'python setup.py sdist',
+            'python setup.py sdist upload',
+        ])
+
+    def test_setup(self):
         runner = self.create_runner()
         pass
 
